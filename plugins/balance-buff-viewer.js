0a629cb601528b2c7410ff3ab59545007fadc9b1
joancio12 <132505856+joancio12@users.noreply.github.com>
2024-03-14 13:26:54 -0300

    Add files via upload

---
+++ plugins/balance-buff-viewer.js
@@ -0,0 +1,9 @@
+/**
+ * @name balance-buff-viewer-plugin
+ * @version 13.19.1
+ * @description ARAM/URF balance buff viewer plugin for Pengu Loader.
+ * @author nomi-san
+ * @link https://github.com/nomi-san/balance-buff-viewer
+ */
+
+export * from 'https://cdn.skypack.dev/balance-buff-viewer-plugin?min';
